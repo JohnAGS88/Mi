f6126104ae7d3ff9de962d70ada4697065eaf3e1
JohnGoyo <jgoyosilva@gmail.com>
2020-08-11 14:21:47 -0400

    version 1

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <form action="">
+      <div>
+      <HelloWorld/>
+          </div>
+            <div class="container mr-4">
+              <div class="columns">
+                <div class="column is-4 is-offset-4">
+                  <h3 class="title is-5">WEEK</h3>
+                    <b-field label="Select a date">
+                      <b-datepicker
+                        placeholder="Type or select a date..."
+                        icon="calendar-today"
+                        editable>
+                    </b-datepicker>
+                  </b-field>
+                <div class="field">
+                  <label class="label">One Week</label>
+                <div class="control">
+                  <input class="input" type="number" v-model.number="ValorA"  placeholder="0">
+                </div>
+                  <label class="label">Two Weeks</label>
+                <div class="control">
+                  <input class="input" type="number" v-model.number="ValorB"  placeholder="0">
+                </div>
+                  <label class="label">Tree Weeks</label>
+                <div class="control">
+                  <input class="input" type="number" v-model.number="ValorC" placeholder="0">
+                </div>
+                  <label class="label">Four Weeks</label>
+                <div class="control">
+                  <input class="input" type="number" v-model.number="ValorD" placeholder="0">
+                </div> 
+                <div>
+                  <p>Total {{ suma() }}<p/>
+                </div>
+                  <button type="summit" class="button is-primary mr-1" >Close</button>
+                  <button type="summit" class="button is-danger" >Saves Changes</button>
+              </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {  
+  name: 'Form',
+  data(){ 
+    return{
+      ValorA: '',
+      ValorB: '',
+      ValorC: '',
+      ValorD: '',
+    }
+  },
+  methods:{
+    suma:function(){
+        return "Decline:"+(this.ValorA+this.ValorB+this.ValorC+this.ValorD)
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+<section class="hero is-light">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Mi Prueba
+      </h1>
+      <h2 class="subtitle has-text-centered">
+        Users
+      </h2>
+    </div>
+  </div>
+
+ </section> 
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  
+</style>
